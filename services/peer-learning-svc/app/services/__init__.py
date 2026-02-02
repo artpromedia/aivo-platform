e6065655e78f301963f8f3b4433b7fe42fd85399
artpromedia <ofemekapongofem@gmail.com>
2026-02-02 09:18:55 -0600

    feat(peer-learning-svc): export new modules from packages
    - Export safety classes from models
    - Export session management classes from models
    - Export peer assessment classes from models
    - Export WebSocketManager from services

--- services/peer-learning-svc/app/services/__init__.py
+++ services/peer-learning-svc/app/services/__init__.py
@@ -1,5 +1,19 @@
 """Peer Learning Services"""
 
 from .matching_engine import MatchingEngine
+from .websocket_manager import (
+    WebSocketManager,
+    WebSocketMessage,
+    MessageType,
+    ConnectionInfo,
+    ws_manager,
+)
 
-__all__ = ["MatchingEngine"]
+__all__ = [
+    "MatchingEngine",
+    "WebSocketManager",
+    "WebSocketMessage",
+    "MessageType",
+    "ConnectionInfo",
+    "ws_manager",
+]
